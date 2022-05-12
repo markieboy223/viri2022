c242074eeab43e9081412d739854c11a732e8c14
justinschouten <j.schouten_97@hotmail.com>
2022-05-12 17:28:42 +0200

    remove portfolio and changed min-heights of section blocks

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -8,8 +8,6 @@
         <a href="/#home" class="JMSoft-bar-item JMSoft-button">HOME</a>
         <a href="/#about" class="JMSoft-bar-item JMSoft-button JMSoft-hide-small"><i class="fa fa-user"></i>
             ABOUT</a>
-        <a href="/#portfolio" class="JMSoft-bar-item JMSoft-button JMSoft-hide-small"><i class="fa fa-th"></i>
-            PORTFOLIO</a>
         <a href="/#services" class="JMSoft-bar-item JMSoft-button JMSoft-hide-small"><i class="fa fa-th"></i>
             SERVICES</a>
         <a href="/companys" class="JMSoft-bar-item JMSoft-button JMSoft-hide-small"><i class="fa fa-building"></i>
@@ -24,7 +22,6 @@
     <!-- Navbar on small screens -->
     <div id="navDemo" class="JMSoft-bar-block JMSoft-white JMSoft-hide JMSoft-hide-large JMSoft-hide-medium">
         <a href="#about" class="JMSoft-bar-item JMSoft-button" onclick="toggleFunction()">ABOUT</a>
-        <a href="#portfolio" class="JMSoft-bar-item JMSoft-button" onclick="toggleFunction()">PORTFOLIO</a>
         <a href="#contact" class="JMSoft-bar-item JMSoft-button" onclick="toggleFunction()">CONTACT</a>
         <a href="#" class="JMSoft-bar-item JMSoft-button">SEARCH</a>
     </div>
